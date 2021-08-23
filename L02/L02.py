b25411377a7ac0195ee58610e949a72baa2de816
javiermomc <javiermomc@outlook.com>
2021-08-23 14:18:15 -0500

    Added author name

--- L02/L02.py
+++ L02/L02.py
@@ -1,3 +1,7 @@
+# Author: Javier Mondragon
+# Institution: ITESM
+# ID: A01365137
+
 import time
 import grovepi
 from grovepi import *
@@ -16,11 +20,11 @@ while True:
 	try:
 		button_status= grovepi.digitalRead(button)	#Read the Button status
 		if not button_status:	#If the Button is in HIGH position, run the program
-			setText_norefresh("Presiona el    \nboton     ")
+			setText_norefresh("Presiona el    \nboton      ")
                         setRGB(255,0,0)
 		else:		#If Button is in Off position, print "Off" on the screen
 			temp = grovepi.temp(sensor,'1.1')
-			setText_norefresh("Temp = {}".format(str(temp)))
+			setText_norefresh("Temp = {} C".format(str(temp)))
 			setRGB(0,255,0)
 	except KeyboardInterrupt:	# Stop the buzzer before stopping
 		break
