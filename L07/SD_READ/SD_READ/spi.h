d74127c839ebada3aa3b10f774d6369aefacec07
javiermomc <javiermomc@outlook.com>
2021-11-02 03:53:22 -0600

    Lab 7 added.

---
+++ L07/SD_READ/SD_READ/spi.h
@@ -0,0 +1,11 @@
+#ifndef SPI_H_
+#define SPI_H_
+
+#define BAUDRATE 9600
+
+void spiInit( void );
+uint8_t spiSend( uint8_t data );
+uint32_t spiXchg(const uint8_t * send_buff, uint32_t bc, uint8_t * receive_buff );
+#include "myprintf.h"
+
+#endif /* SPI_H_ */
